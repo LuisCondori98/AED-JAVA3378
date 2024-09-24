e8bfb7c9cc5baa81dcdb68c0472efb392c39dba4
unknown <SUITE@SUITECE215-ST15.cibertec.edu.pe>
2024-09-24 08:29:36 -0500

    corrigiendo guis

--- Evaluacion/T1/src/jframes/Problema1.java
+++ Evaluacion/T1/src/jframes/Problema1.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Problema1 extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnProblema1;
+	private JTextArea txtProblema1;
 
 	/**
 	 * Launch the application.
@@ -32,11 +38,28 @@ public class Problema1 extends JFrame {
 	 */
 	public Problema1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 517, 554);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnProblema1 = new JButton("Procesar");
+		btnProblema1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				actionPerformedBtnProblema1(arg0);
+			}
+		});
+		btnProblema1.setBounds(209, 11, 89, 23);
+		contentPane.add(btnProblema1);
+		
+		txtProblema1 = new JTextArea();
+		txtProblema1.setBounds(10, 38, 481, 466);
+		contentPane.add(txtProblema1);
 	}
-
+	
+	protected void actionPerformedBtnProblema1(ActionEvent arg0) {
+		
+		
+	}
 }
