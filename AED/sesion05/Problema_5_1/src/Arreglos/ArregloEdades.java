73f7c5f05301707626325882574aa479b83e3958
unknown <SUITE@SUITECE215-ST15.cibertec.edu.pe>
2024-09-24 12:05:30 -0500

    comitea ctm

---
+++ AED/sesion05/Problema_5_1/src/Arreglos/ArregloEdades.java
@@ -0,0 +1,18 @@
+package Arreglos;
+
+public class ArregloEdades {
+
+	private int[] arr = {27, 22, 13, 12, 25, 11, 29, 70, 15, 21};
+	
+	public int tamanio() {
+		
+		return arr.length;
+	}
+	
+	public int obtener(int i) {
+		
+		return arr[i];
+	}
+	
+	
+}
