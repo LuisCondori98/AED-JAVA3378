c81235d2654aceb1e9135b49d17cae8ed729bf50
unknown <SUITE@SUITECE215-ST13.cibertec.edu.pe>
2024-10-01 12:00:29 -0500

    clase07

--- AED/sesion07/Problema_7_1/src/semana_07/ArregloEdades.java
+++ AED/sesion07/Problema_7_1/src/semana_07/ArregloEdades.java
@@ -38,4 +38,57 @@ public class ArregloEdades {
 			edad[i] = aux[i];
 	}
 
+	public int posPrimeraEdadAdolescente() {
+		
+		for(int i = 0; i < tamanio(); i++) {
+			
+			if(edad[i] >= 12 && edad[i] <= 20) {
+				
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public int posUltimaEdadAdolescente() {
+		
+		for(int i = tamanio() - 1; i >= 0; i--) {
+			
+			if(edad[i] >= 12 && edad[i] <= 20) {
+				
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void remplazarPrimeraEdadAdolescente() {
+		
+		edad[posPrimeraEdadAdolescente()] = edad[tamanio() - 1];
+	}
+	
+	private void intercambiar(int pos1, int pos2) {
+		
+		int aux = edad[pos1];
+		edad[pos1] = edad[pos2];
+		edad[pos2] = aux;
+	}
+	
+	public void intercambiarEdadesAdolescentesExtremas() {
+		
+		intercambiar(posPrimeraEdadAdolescente(), posUltimaEdadAdolescente());
+	}
+	
+	private void eliminar(int pos) {
+		
+		for(int i = pos; i < indice; i++) {
+			
+			edad[i] = edad[i+1];
+		}
+	}
+	
+	public void eliminarPrimeraEdadAdolescente() {
+		
+		eliminar(posPrimeraEdadAdolescente());
+	}
 }
